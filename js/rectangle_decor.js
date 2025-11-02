666743e166d52cc2c5ea0925ef17ac8871065967
monions <monions@uiowa.edu>
2025-11-02 14:41:25 -0600

    bug fixes phone responsiveness

--- js/rectangle_decor.js
+++ js/rectangle_decor.js
@@ -1,27 +1,20 @@
 
-window.addEventListener('resize', function() {
-    console.log('changing height');
+// window.addEventListener('resize', function() {
+//     console.log('changing height');
 
-    // Get the rectangle element
-    var rectangle = document.getElementById('project-decor'); 
+//     // Get the rectangle element
+//     var rectangle = document.getElementById('project-decor'); 
 
-    // Get the y position of the bottom-most card
-    var cardElement = document.getElementById('last-card'); 
-    var cardRect = cardElement.getBoundingClientRect();
-    var cardBottom = cardRect.bottom;
+//     // Get the y position of the bottom-most card
+//     var cardElement = document.getElementById('last-card'); 
+//     var cardRect = cardElement.getBoundingClientRect();
+//     var cardBottom = cardRect.bottom;
 
-    // Get the y position of the footer
-    var footerElement = document.getElementById('last-card'); 
-    var footerRect = footerElement.getBoundingClientRect();
-    var footerTop = footerRect.top + this.window.scrollY;
+//     var subtractedVH = window.innerHeight / 2;
 
-    console.log(footerTop);
+//     rectangle.style.height = (cardBottom * 2) + "px";
 
-    var subtractedVH = window.innerHeight / 2;
-
-    rectangle.style.height = (footerTop - subtractedVH) + "px";
-
-    // Adjust height based on the bottom-most card
-    // rectangle.style.height = scrollY;
-    // console.log('rectangle height:' + rectangle.style.height);
-  });
+//     // Adjust height based on the bottom-most card
+//     // rectangle.style.height = scrollY;
+//     // console.log('rectangle height:' + rectangle.style.height);
+//   });
